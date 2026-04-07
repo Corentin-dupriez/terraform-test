5d7b34bbeaf514e35cd69585f7eef5216f622c20
Corentin Dupriez <dupriez.c@icloud.com>
2026-04-07 19:17:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ resource "azurerm_resource_group" "arg" {
 
 resource "azurerm_mssql_server" "mssql" {
   name                         = var.sql_server_name
-  resource_group_name          = var.resource_group_name
-  location                     = var.resource_group_location
+  resource_group_name          = azurerm_resource_group.arg.name      
+  location                     = azurerm_resource_group.arg.location  
   version                      = "12.0"
   administrator_login          = var.sql_admin_login
   administrator_login_password = var.sql_admin_password
@@ -54,22 +54,24 @@ resource "azurerm_mssql_firewall_rule" "firewall" {
 
 resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.arg.name      
+  location            = azurerm_resource_group.arg.location  
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "alwa" {
   name                = var.app_service_name
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.arg.name      
+  location            = azurerm_resource_group.arg.location  
+  service_plan_id     = azurerm_service_plan.asp.id
+
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
     value = "Data Source=tcp:${azurerm_mssql_server.mssql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.mssql_db.name};User ID=${azurerm_mssql_server.mssql.administrator_login};Password=${azurerm_mssql_server.mssql.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
-  service_plan_id = azurerm_service_plan.asp.id
+
   site_config {
     application_stack {
       dotnet_version = "6.0"
